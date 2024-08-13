9df3a5bb37c93c8abb43786f26e2f6f67bbe5c45
Tatenda Mudehwe <tmudehwe@hotmail.com>
2024-08-13 06:27:49 +0000

    Advanced Routing

--- run.py
+++ run.py
@@ -25,6 +25,17 @@ def about():
    return render_template("about.html", page_title="About", company=data) # remember to leave two spaces between python functions
 
 
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return render_template("member.html", member=member)
+
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html", page_title="contact")# remember to leave two spaces between python functions
